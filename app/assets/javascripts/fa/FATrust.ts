1ab30d4ba216a3c21f597ab809934e7a2f7ccd06
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-09-25 13:29:25 +0200

    This should fix yarn integrity check failed

--- app/assets/javascripts/fa/FATrust.ts
+++ app/assets/javascripts/fa/FATrust.ts
@@ -1,6 +1,9 @@
 export default class FATrust {
+    // How many peptides are annotated with at least a term?
     public annotatedCount: number;
+    // TODO: what's this for?
     public totalCount: number;
+    // This is the total amount of peptides that were looked up for a specific search.
     public trustCount: number;
 
     constructor(annotatedCount: number, totalCount: number, trustCount: number) {
